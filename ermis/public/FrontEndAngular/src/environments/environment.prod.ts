191047d18214b190e22f90e69aaa4ff9013a2512
MbalamulaS <nyanditi.mbalamula@gmail.com>
2022-04-10 03:37:36 +0300

    removed old files

--- ermis/public/FrontEndAngular/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  // baseURL: 'http://localhost:8000/',
-  // //baseURL: 'http://41.59.90.211:8000/',
-  // leaderImgURL: 'http://localhost:4200/assets/images/leaders/',
-  //production: false,
-
-  baseURL: window["env"]["baseURL"] || "default",
-  production: window["env"]["production"] || "default"
-};
