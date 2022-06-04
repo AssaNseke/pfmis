dd5fdb97d2d359a9aa44ca9d9f0057c6089d0650
MbalamulaS <nyanditi.mbalamula@gmail.com>
2022-06-04 11:47:22 +0300

    employee model, migration and relations defined

--- ermis/app/Models/Employee.php
+++ ermis/app/Models/Employee.php
@@ -4,8 +4,42 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Employee extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+
+
+    protected $fillable = [
+        'emp_name',
+        'emp_number',
+        'dept_id',
+        'file_id',
+        'contact_id',
+        'category_id',
+        'status'
+
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    public function contact(){
+        return $this->belongsTo(Contact::class, 'contact_id');
+    }
+    public function department(){
+        return $this->belongsTo(department::class, 'dept_id');
+    }
+
+    public function file(){
+        return $this->belongsTo(File::class, 'file_id');
+    }
+
+    public function employeeCategory(){
+        return $this->belongsTo(employeeCategory::class, 'category_id');
+    }
+
 }
--- ermis/database/migrations/2022_06_02_154352_create_employees_table.php
+++ ermis/database/migrations/2022_06_02_154352_create_employees_table.php
@@ -16,8 +16,35 @@ class CreateEmployeesTable extends Migration
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
             $table->string('emp_name');
-            $table->string('emp_dept');
-            $table->string('emp_file');
+            $table->string('emp_number')->nullable();
+
+            $table->integer('dept_id')->unsigned();
+            $table->foreign('dept_id')
+            ->references('id')->on('departments')
+            ->onUpdate('cascade')
+            ->onDelete('restrict');
+
+            $table->integer('file_id')->unsigned();
+            $table->foreign('file_id')
+            ->references('id')->on('files')
+            ->onUpdate('cascade')
+            ->onDelete('restrict');
+
+            $table->integer('contact_id')->unsigned();
+            $table->foreign('contact_id')
+            ->references('id')->on('contacts')
+            ->onUpdate('cascade')
+            ->onDelete('restrict');
+
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')
+            ->references('id')->on('employee_categories')
+            ->onUpdate('cascade')
+            ->onDelete('restrict');
+
+            $table->string('status')->default('Active');
+
+
             $table->timestamps();
             $table->timestamp('deleted_at')->nullable();
         });
